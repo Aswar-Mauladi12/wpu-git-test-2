7c014e5a18856c8cef0c46e3effed988adfb3767
Aswar-Mauladi12 <aswarmauladi@gmail.com>
2023-07-11 14:36:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolor atque maiores modi, fuga nisi, recusandae explicabo, sint hic impedit error asperiores? Ratione porro magni laborum exercitationem soluta. Expedita, laborum.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
